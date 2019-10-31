70f667a691fc4799944a56815ccb68cc70d287e7
Gregory D Hardy <gdhardy1@gmail.com>
2019-10-31 10:26:20 -0700

    rename directory to libraries

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "./libaries/materialize/sass/materialize.scss";
-import "./libaries/materialize/js/materialize.min.js";
+import "./libraries/materialize/sass/materialize.scss";
+import "./libraries/materialize/js/materialize.min.js";
 import "./tooltip.js";
 import "./styles/styles.scss";
