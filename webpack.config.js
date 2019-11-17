7d11bfd2614345cf4926375ef318ef6dff16fcf0
Gregory D Hardy <gdhardy1@gmail.com>
2019-11-17 04:15:08 -0800

    split code

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,54 @@
 const path = require("path");
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.ts",
+	entry: {
+		index: "./index.ts",
+		materialize: "./libraries/materialize/sass/materialize.scss",
+		styles: "./styles/styles.scss",
+	},
+	context: path.resolve(__dirname, "src"),
+	mode: "development",
 	devtool: "inline-source-map",
 	devServer: {
-		contentBase: "./dist"
+		contentBase: "./dist",
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			title: "Password Generator",
-			template: "./src/index.html"
-		})
+			template: "index.html",
+		}),
+		new webpack.HashedModuleIdsPlugin({
+			hashFunction: "sha256",
+			hashDigest: "hex",
+			hashDigestLength: 20,
+		}),
 	],
+	optimization: {
+		splitChunks: {
+			chunks: "all",
+		},
+	},
 	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "dist")
+		filename: "[name].[contenthash:8].js",
+		path: path.resolve(__dirname, "dist"),
 	},
 	module: {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: ["style-loader", "css-loader", "sass-loader"]
+				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 			{ test: /\.css$/, use: ["style-loader, css-loader"] },
 			{
 				test: /\.tsx?$/,
 				use: "ts-loader",
-				exclude: /node_modules/
-			}
-		]
+				exclude: /node_modules/,
+			},
+		],
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js"]
-	}
+		extensions: [".tsx", ".ts", ".js"],
+	},
 };
